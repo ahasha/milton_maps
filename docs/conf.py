36a8a5c23859187392d52deea59bf07016b38961
Alex Hasha <ahasha@gmail.com>
2023-02-16 18:11:44 -0500

    Built rebranded docs

--- docs/conf.py
+++ docs/conf.py
@@ -100,6 +100,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
 html_logo = "color_logo_with_background.png"
+html_favicon = 'favicon.ico'
 
 html_scaled_image_link = False
 
